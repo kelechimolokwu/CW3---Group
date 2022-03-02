1caeb705ce11a87f6639705e63b699a2ec8f2389
Kelechi Molokwu <kelechimolokwu@gmail.com>
2022-03-02 16:59:17 +0000

    Created Service Worker

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+  'index.html',
+  'lessons.js',
+  'afterschool.webmanifest',
+  'images/biology.png',
+  'images/chemistry.png',
+  'images/math.png',
+  'images/physics.png',
+  'images/icon-store-512.png',
+  'images/afterschool.png',
+  'images/favicon.png'
+];
+
+// CACHING THE FILES IN THE SERVICE WORKER [cacheFiles] ARRAY
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install')
+    e.waitUntil (caches.open(cacheName).then((cache) => {
+      console.log('[Service Worker] Caching all files')
+      return cache.addAll(cacheFiles)
+    }))
+  })
+
+self.addEventListener('fetch', (e) => {
+    console.log('[Service Worker] Fetched resource '+e.request.url);
+});
+
+// USING THE CACHED FILES
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    // check if the cache has the file
+    caches.match(e.request).then((r) => {
+      console.log('[Service Worker] Fetching resource: ' + e.request.url)
+      return r
+    })
+  )
+})
+
+//   CACHING NEW FILES
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+      caches.match(e.request).then(function (r) {
+        // Download the file if it is not in the cache,
+        return r || fetch(e.request).then(function (response) {
+          // add the new file to cache
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+  }); });
+  }) );
+  });

52b46b21500be18f46e558f747b4b47befc38d1b
Kelechi Molokwu <kelechimolokwu@gmail.com>
2022-03-02 23:09:04 +0000

    Service Worker Caching

--- service-worker.js
+++ service-worker.js
@@ -14,33 +14,66 @@ var cacheFiles = [
 ];
 
 // CACHING THE FILES IN THE SERVICE WORKER [cacheFiles] ARRAY
+// self.addEventListener('install', (e) => {
+//     console.log('[Service Worker] Install')
+//     e.waitUntil (caches.open(cacheName).then((cache) => {
+//       console.log('[Service Worker] Caching all files')
+//       return cache.addAll(cacheFiles)
+//     }))
+//   })
+
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install')
-    e.waitUntil (caches.open(cacheName).then((cache) => {
-      console.log('[Service Worker] Caching all files')
-      return cache.addAll(cacheFiles)
-    }))
-  })
+    console.log('[servie worker] install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[service worker] caching files.');
+            return cache.addAll(cacheFiles);
+        })
+    )
+});
   
 //   USING THE CACHED FILES
-  self.addEventListener('fetch', function (e) {
+//   self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//       // check if the cache has the file
+//       caches.match(e.request).then((r) => {
+//         console.log('[Service Worker] Fetching resource: ' + e.request.url)
+//         return r
+//       })
+//     )
+//   })
+
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-      // check if the cache has the file
-      caches.match(e.request).then((r) => {
-        console.log('[Service Worker] Fetching resource: ' + e.request.url)
-        return r
-      })
-    )
-  })
+        caches.match(e.request).then(function (r) {
+        // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+            // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                    });
+                });
+            })
+        );
+    });
   
   // CACHING NEW FILES
-  self.addEventListener('fetch', function (e) {
-    e.respondWith(caches.match(e.request).then(function (r) {
-      // Download the file if it is not in the cache
-      return r || fetch(e.request).then(function (response) {
-        // add the new file to cache
-        cache.put(e.request, response.clone())
-        return response;
-      })
-    }))
-  })
+//   self.addEventListener('fetch', function (e) {
+//     e.respondWith(caches.match(e.request).then(function (r) {
+//       // Download the file if it is not in the cache
+//       return r || fetch(e.request).then(function (response) {
+//         // add the new file to cache
+//         cache.put(e.request, response.clone())
+//         return response;
+//       })
+//     }))
+//   })
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r){
+            return r
+        })
+    )
+});

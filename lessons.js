1b0c3b6eaca48debdfa802e7e39ddc74a362fc74
Kelechi Molokwu <kelechimolokwu@gmail.com>
2022-03-02 15:25:08 +0000

    Generated Web Manifest

---
+++ lessons.js
@@ -0,0 +1,38 @@
+const lessons = [
+    {
+      id: 1001,
+      title: "Math",
+      description: "Math lessons. Guaranteed to make them cry.",
+      price: 2000,
+      image: "math.png",
+      availableInventory: 10,
+      rating: 1
+    },
+    {
+      id: 1002,
+      title: "Biology",
+      description: "A better understanding of life.",
+      price: 2000,
+      image: "biology.png",
+      availableInventory: 10,
+      rating: 3
+    },
+    {
+      id: 1003,
+      title: "Chemistry",
+      description: "We make things go boom.",
+      price: 2000,
+      image: "chemistry.png",
+      availableInventory: 10,
+      rating: 2
+    },
+    {
+      id: 1004,
+      title: "Physics",
+      description: "You might be the next Albert Einstein.",
+      price: 2000,
+      image: "physics.png",
+      availableInventory: 10,
+      rating: 5
+    }
+  ]
